24fca5d43cce2a395384f01dca6b9cad8ccca7c3
Michał Woźny <mbwozny@gmail.com>
2019-09-14 21:12:54 +0200

    Added SZA tests

--- Akademia_PWSW/SZA/code/generate.cpp
+++ Akademia_PWSW/SZA/code/generate.cpp
@@ -7,14 +7,14 @@ int main(){
     mt19937 gen(rd());
 
     ios_base::sync_with_stdio(0);
-    uniform_int_distribution<int> dist_n(200000, 200000);
+    uniform_int_distribution<int> dist_n(199950, 200000);
     int n = dist_n(gen);
-    uniform_int_distribution<int> dist_m(0, 1000000000);
+    uniform_int_distribution<int> dist_m(7*1e8, 1e9);
     int m = dist_m(gen);
     cout << n << " " << m << "\n";
 
     for(int i=0; i<n; i++){
-        uniform_int_distribution<int> dist_r(1, 1000000000);
+        uniform_int_distribution<int> dist_r(48974659, 49157182);
         int r = dist_r(gen);
         cout << r << " ";
     } cout << "\n";
