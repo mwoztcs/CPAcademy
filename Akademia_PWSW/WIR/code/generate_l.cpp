1c3e0b842eb2dd90b529b10bd37e74fcddfe701a
Michał Woźny <mbwozny@gmail.com>
2019-09-20 22:11:05 +0200

    Added WIR tests

--- Akademia_PWSW/WIR/code/generate_l.cpp
+++ Akademia_PWSW/WIR/code/generate_l.cpp
@@ -7,14 +7,23 @@ int main(){
     
     ios_base::sync_with_stdio(0);
     
-    uniform_int_distribution<int> dist_n(190000, 2e5);
+    uniform_int_distribution<int> dist_n(199000, 2e5);
     int n = dist_n(gen);
-    uniform_int_distribution<int> dist_m(1, n/10000);
+    uniform_int_distribution<int> dist_m(1, n);
     int m = dist_m(gen);
     cout << n << " " << m << "\n";
     
-    for(int i=2; i<=n; i++){
+    uniform_int_distribution<int> dist_div(10, n-10);
+    int div = dist_div(gen);
+    
+    for(int i=2; i<=div; i++){
         uniform_int_distribution<int> dist_u(1, i-1);
+        int u = dist_u(gen);
+        cout << i << " " << u << " 0\n";
+    }
+    
+    for(int i=div+1; i<=n; i++){
+        uniform_int_distribution<int> dist_u(1, div);
         int u = dist_u(gen);
         uniform_int_distribution<int> dist_a(1, 1);
         int a = dist_a(gen);
