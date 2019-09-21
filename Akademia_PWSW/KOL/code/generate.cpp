bd62396b19d6a83b1941d02cc737f6581b53d9ce
Michał Woźny <mbwozny@gmail.com>
2019-09-22 00:48:47 +0200

    Added KOL tests

--- Akademia_PWSW/KOL/code/generate.cpp
+++ Akademia_PWSW/KOL/code/generate.cpp
@@ -7,20 +7,27 @@ int main(){
 
     ios_base::sync_with_stdio(0);
 
-    uniform_int_distribution<int> dist_n(100000, 100000);
+    uniform_int_distribution<int> dist_n(1e5, 1e5);
     int n = dist_n(gen);
-    uniform_int_distribution<int> dist_m(100000, 100000);
+    uniform_int_distribution<int> dist_m(1e5, 1e5);
     int m = dist_m(gen);
     cout << n << " " << m << "\n";
 
-    for(int i=0; i<n; i++){
-        uniform_int_distribution<int> dist_h(49981, 50000);
+    vector<int> V;
+    for(int i=0; i<10; i++){
+        uniform_int_distribution<int> dist_h(1, 2e4);
         int h = dist_h(gen);
-        cout << h << " ";
+        V.push_back(h);
+    }
+    
+    for(int i=0; i<n; i++){
+        uniform_int_distribution<int> dist_v(0, 9);
+        int v = V[dist_v(gen)];
+        cout << v << " ";
     } cout << "\n";
 
     for(int i=0; i<m; i++){
-        uniform_int_distribution<int> dist_c(1, 100000);
+        uniform_int_distribution<int> dist_c(1, 1e5);
         int c = dist_c(gen);
         cout << c << " ";
     } cout << "\n";
